f4563152a3a96e8bdbad9b4568f922653a39e8d9
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-02-28 17:25:42 +0530

    made little

---
+++ src/footer/footer sections/bottomsec.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bottomsec = () => {
+  return (
+    <>
+        <div className='w-screen bg-[#010101] h-10 text-xs md:text-lg border-t border-white md:h-20 grid place-items-center text-white'><div>Copyright 2023 @CarMania pvt.ltd</div></div>
+    </>
+  )
+}
+
+export default Bottomsec
---
+++ src/footer/footer sections/midsec.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Midsec = () => {
+  return (
+    <>
+        <div className='w-screen h-32 md:h-56 bg-black'>
+            
+        </div>
+    </>
+  )
+}
+
+export default Midsec
